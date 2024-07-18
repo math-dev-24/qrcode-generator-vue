f68ab448cda5783bf3e721024fe93537dc6b82f4
Mathieu <mathieu.busse@live.fr>
2024-07-18 11:27:30 +0200

    update: design for no qrcode

--- src/views/ListQrView.vue
+++ src/views/ListQrView.vue
@@ -4,10 +4,11 @@
     <div class="grid grid-cols-3 gap-4" v-if="useQrStore().list.length > 0">
       <QRCard  v-for="qr in useQrStore().list" :key="qr.id" :qr="qr" />
     </div>
-    <div v-else class="text-center text-2xl font-bold border p-16 my-6 bg-stone-200 rounded flex flex-col items-center justify-center">
-      <span>Aucun QR</span>
-      <router-link to="/" class="text-stone-500 hover:text-black hover:underline">
-        <span class="text-sm">Créer un QR</span>
+    <div v-else class="text-center font-bold border p-16 my-6 bg-stone-200 rounded flex flex-col items-center justify-center hover:drop-shadow-lg transition-all duration-300">
+      <span class="text-2xl">Aucun QR Code créé</span>
+      <router-link to="/" class="text-stone-500 hover:text-black text-xl mt-4 flex items-center justify-center gap-1">
+        <PlusCircleIcon class="size-6" />
+        <span>Créer un QR Code</span>
       </router-link>
     </div>
   </section>
@@ -15,4 +16,5 @@
 <script lang="ts" setup>
 import { useQrStore } from '@/stores/QrStore'
 import QRCard from '@/components/QRCard.vue'
+import { PlusCircleIcon } from '@heroicons/vue/24/solid'
 </script>
