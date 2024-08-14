8423be221d4f20ea798e78fb679ae43ed8a3c541
Mathieu <mathieu.busse@live.fr>
2024-08-14 11:25:04 +0200

    update: design header

--- src/components/Template/Header.vue
+++ src/components/Template/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex py-1 md:py-2 px-2 sm:px-8 md:px-14 lg:px-16 justify-between">
+  <header class="flex py-1 md:py-2 px-2 sm:px-8 md:px-10 lg:px-14 bg-white justify-between drop-shadow-xl border-b border-b-gray-200">
     <h1 class="flex-1 text-lg md:text-xl"><router-link to="/">QR CODE GENERATOR</router-link></h1>
     <ul class="flex text-lg md:text-xl">
       <li><router-link to="/">Générer</router-link></li>
