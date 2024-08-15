7ddc2fb5619d75826bac04d585e849722802f83c
Mathieu <mathieu.busse@live.fr>
2024-08-15 10:55:22 +0200

    new: mise en place code phone for sms

--- src/components/form/Sms.vue
+++ src/components/form/Sms.vue
@@ -2,11 +2,14 @@
   <div class="flex flex-col gap-4">
     <div class="form_group">
       <label for="phone">Numéro de téléphone : <span class="text-red-600">*</span></label>
-      <input type="text" id="phone" v-model="phone" placeholder="+33645842524" />
+      <div class="flex flex-row gap-2">
+        <SelectCountryPhone v-model:code="code"/>
+        <input type="text" id="phone" v-model="phone" placeholder="645842524" class="flex-1" />
+      </div>
     </div>
     <div class="form_group">
       <label for="message">Message : <span class="text-red-600">*</span></label>
-      <textarea type="text" id="message" v-model="message" placeholder="Bonjour, ..." />
+      <textarea type="text" id="message" v-model="message" placeholder="Bonjour, très long message ..." />
     </div>
     <ButtonForm @generateQr="generateQr" />
   </div>
@@ -18,8 +21,10 @@ import { useQrStore } from '@/stores/QrStore'
 import { smsSend } from '@/shared/SmsSend'
 import ButtonForm from '@/components/form/ButtonForm.vue'
 import { checkPhone } from '@/shared/validator'
+import SelectCountryPhone from '@/components/input/SelectCountryPhone.vue'
 
 const phone = ref<string>("")
+const code = ref<string>("+33")
 const message = ref<string>("")
 
 const emit = defineEmits<{
@@ -35,12 +40,13 @@ watch([phone, message], () => {
 })
 
 function generateQr () {
-  const sms_str: string = smsSend(phone.value, message.value)
+  const tmp_phone: string = code.value + phone.value
+  const sms_str: string = smsSend(tmp_phone, message.value)
   emit('generateQr', sms_str)
 }
 
 function checkForm() {
-  if(phone.value.length === 0 || message.value.length === 0) {
+  if(phone.value.length === 0 || message.value.length === 0 || code.value.length === 0) {
     useQrStore().addAlert("Données d'entrée manquantes")
   }else{
     useQrStore().removeAlert("Données d'entrée manquantes")
