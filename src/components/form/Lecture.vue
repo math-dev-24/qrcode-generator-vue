57457f98ca5c31792e36e326fffb9e49bb3f4693
Mathieu <mathieu.busse@live.fr>
2024-10-12 15:00:41 +0200

    add stat vercel

--- src/components/form/Lecture.vue
+++ src/components/form/Lecture.vue
@@ -5,14 +5,14 @@
     <div class="rounded border outline outline-1 outline-green-300 px-2 py-4 mt-4 bg-green-100 text-green-700">
       <p>
         <span class="font-bold">Type : </span>
-        <span>{{ detectQRCodeType(qrCodeData) }}</span>
+        <span>{{ type }}</span>
       </p>
       <p v-if="qrCodeData">
         <span class="font-bold">Données sur le QR Code : </span>
         <span>{{ qrCodeData }}</span>
       </p>
       <p v-else-if="qrCodeData === ''">Pas de QR Code détecté</p>
-      <p v-else>{{ qrCodeData }}</p>
+      <p v-else v-html="qrCodeData"></p>
     </div>
   </div>
 </template>
@@ -23,6 +23,7 @@ import jsQR from 'jsqr'
 
 
 const qrCodeData = ref<string | null>(null)
+const type = ref<string>("twitter")
 
 
 function detectQRCodeType(data: string | null): string {
@@ -60,6 +61,33 @@ function detectQRCodeType(data: string | null): string {
   return 'Unknown'
 }
 
+function formatVCard(vcardStr: string) {
+  vcardStr = vcardStr.replace('BEGIN:VCARD', '').replace('END:VCARD', '').trim();
+  vcardStr = vcardStr.replace('VERSION:3.0', '');
+
+  const fields = {
+    Nom: vcardStr.match(/N:([^;]+);/),
+    Prénom: vcardStr.match(/FN:[^;]+;([^;]+)/),
+    Titre: vcardStr.match(/TITLE:([^\n]+)/),
+    Téléphone: vcardStr.match(/TEL;TYPE=Work:([^\n]+)/),
+    Email: vcardStr.match(/EMAIL;TYPE=PREF,INTERNET:([^\n]+)/),
+    Adresse: vcardStr.match(/ADR;TYPE=WORK:([^\n]+)/),
+    URL: vcardStr.match(/URL:([^\n]+)/),
+    Organisation: vcardStr.match(/ORG:([^\n]+)/)
+  };
+
+  let output = "";
+  if (fields.Nom) output += `Nom: ${fields.Nom[1]}\n`;
+  if (fields.Prénom) output += `Prénom: ${fields.Prénom[1]}\n`;
+  if (fields.Titre) output += `Titre: ${fields.Titre[1]}\n`;
+  if (fields.Téléphone) output += `Téléphone (Work): ${fields.Téléphone[1]}\n`;
+  if (fields.Email) output += `Email: ${fields.Email[1]}\n`;
+  if (fields.Adresse) output += `Adresse (Work): ${fields.Adresse[1]}\n`;
+  if (fields.URL) output += `URL: ${fields.URL[1]}\n`;
+  if (fields.Organisation) output += `Organisation: ${fields.Organisation[1]}\n`;
+
+  return output.trim();}
+
 function handleFileChange(e: Event) {
   const input = e.target as HTMLInputElement
   if (!input.files?.length) {
@@ -84,7 +112,12 @@ function handleFileChange(e: Event) {
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
         const code = jsQR(imageData.data, canvas.width, canvas.height)
         qrCodeData.value = code ? code.data : ''
-        detectQRCodeType(qrCodeData.value)
+        type.value = detectQRCodeType(qrCodeData.value)
+        if(qrCodeData.value){
+          if (type.value === 'vCard') {
+            qrCodeData.value = formatVCard(qrCodeData.value)
+          }
+        }
       }
       img.src = event.target?.result as string
     }
